266f4e4b0dbd397eb0c3216beaf1de6bc4f0789c
charles milam <charles.milam@gmail.com>
2014-11-21 02:42:41 -0600

    Modify for latest coursework

---
+++ day4_challenge.js
@@ -0,0 +1,11 @@
+function alphaCount (alphabet, text) {
+  // Write your code here, and
+  // return your final answer.
+  alpha = {}
+  
+  for (var i = key.length - 1; i >= 0; i--) {
+  	alpah[key[i].toLowerCase()] += 1
+  };
+
+  
+}
