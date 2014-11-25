fb97db9d6e58aabc18e4738e9a52e02188f8795c
charles milam <charles.milam@gmail.com>
2014-11-25 12:02:53 -0600

    Add creation of classmates table

--- databases.rb
+++ databases.rb
@@ -1,8 +1,12 @@
 require "pg"
 
 # make connection to database
-conn = PG::Connection.open(:dbname => 'mks')
+db = PG::Connection.open(:dbname => 'mks')
 
 # output the connected database name and user
-puts conn.db 
-puts conn.user
+puts db.db 
+puts db.user
+
+# create new table, if it does not already exist
+create_table = "create table if not exists classmates (id serial primary key, first_name varchar, last_name varchar, twitter varchar) "
+db.exec(create_table)
