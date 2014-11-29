ac45e1b6237e7de298f42884f8e227e3d953d4b9
charles milam <charles.milam@gmail.com>
2014-11-28 18:13:55 -0600

    Add params assignment in /info-summary

--- sinatra_intro/app.rb
+++ sinatra_intro/app.rb
@@ -12,4 +12,9 @@ end
 
 post "/info-summary" do
   puts params
+  @name = params[:name]
+  @email = params[:mail]
+  @vegetarian = params[:vegetarian]
+  @blood_type = params[:bloodtype]
+  erb :summary
 end
