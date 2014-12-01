65c6cfa027c7e8eadfa6a324964aed7ed3a9dab8
charles milam <charles.milam@gmail.com>
2014-11-30 20:31:11 -0600

    Initial commit.

---
+++ sql/movies_actors_frequency.sql
@@ -0,0 +1,12 @@
+﻿SELECT 
+  actors.name as Actor, 
+  count(actors_movies.movie_id) as Appearances
+FROM 
+  public.actors_movies, 
+  public.actors
+WHERE 
+  actors_movies.actor_id = actors.id
+GROUP BY
+  actors.name
+ORDER BY
+  actors.name ASC;
