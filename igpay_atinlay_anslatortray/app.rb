a07cbfb5379d9ed0f472bfad936bba19bb996e34
charles milam <charles.milam@gmail.com>
2014-11-30 20:19:59 -0600

    Modify to make use of pig_latin version 0.2.0, and its translate method.

--- igpay_atinlay_anslatortray/app.rb
+++ igpay_atinlay_anslatortray/app.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
+require "pig_latin"
 
 get '/translate' do
   erb :translate
 end
 
 post '/translate' do
+  puts params
+  # pigger = PigLatin.new
+  @word = params[:word]
+  @pigified = PigLatin.translate(@word)
   erb :result
 end
-
