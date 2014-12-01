2a9a9684900d81cec1890a8d1d7ac839e2c8c896
charles milam <charles.milam@gmail.com>
2014-11-30 20:31:33 -0600

    Initial commit.

---
+++ sql/petshop_union.sql
@@ -0,0 +1,10 @@
+﻿SELECT dogs.name as pet_name, petshops.name  
+      FROM dogs  
+      JOIN petshops  
+      ON dogs.shop_id = petshops.id 
+      UNION  
+      SELECT cats.name pet_name, petshops.name  
+      FROM cats  
+      JOIN petshops  
+      ON cats.shop_id = petshops.id 
+      order by pet_name
