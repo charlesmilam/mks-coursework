2c78322844c82726807587ff5d33ba95814f1123
charles milam <charles.milam@gmail.com>
2014-11-30 20:31:22 -0600

    Initial commit.

---
+++ sql/movies_with_actors.sql
@@ -0,0 +1,13 @@
+﻿SELECT 
+  movies.title as movie, 
+  actors.name as actor
+FROM 
+  public.actors, 
+  public.movies_actors, 
+  public.movies
+WHERE 
+  movies_actors.actor_id = actors.id AND
+  movies_actors.movie_id = movies.id
+ORDER BY
+  movies.title ASC, 
+  actors.name ASC;
