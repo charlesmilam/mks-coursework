b8dc9f1d88987964fbce717964237976b2859d5c
charles milam <charles.milam@gmail.com>
2014-12-03 14:30:40 -0600

    Modify index route to retrieve results from movie api. Change name for results variable for clarification.

--- movies/server.rb
+++ movies/server.rb
@@ -5,12 +5,11 @@ require "rest-client"
 api = "http://movies.api.mks.io/movies"
 
 get '/' do
-  puts @api
-  @result = JSON.parse(RestClient.get api)
-  
+  @index_results = JSON.parse(RestClient.get api)
   erb :"index"
 end
 
 get '/movies' do
+
   erb :"movies/index"
 end
