8debe59a26955a96813ed9ec037d9f5400ae51b7
charles milam <charles.milam@gmail.com>
2014-12-03 15:56:17 -0600

    Modify to get specific movie title and associated actors, for /movies end point.

--- movies/server.rb
+++ movies/server.rb
@@ -2,14 +2,19 @@ require "sinatra"
 require "json"
 require "rest-client"
 
-api = "http://movies.api.mks.io/movies"
+api = "http://movies.api.mks.io/movies/"
 
 get '/' do
   @index_results = JSON.parse(RestClient.get api)
   erb :"index"
 end
 
-get '/movies' do
-
-  erb :"movies/index"
+get '/movies/:id' do
+  puts params
+  @movie_result = JSON.parse(RestClient.get api + params["id"])
+  @actors_result = JSON.parse(RestClient.get api + params["id"] + "/actors")
+  @movie_result.each {|movie| puts movie}
+  puts @movie_result["title"]
+  @actors_result.each {|actor| puts actor}
+  erb :"movies"
 end
