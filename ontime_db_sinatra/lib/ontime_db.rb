6883a463bade910f72326505745acbbd0c3f5b4c
charles milam <charles.milam@gmail.com>
2014-12-04 10:37:28 -0600

    Add method avg_delay_all

--- ontime_db_sinatra/lib/ontime_db.rb
+++ ontime_db_sinatra/lib/ontime_db.rb
@@ -73,9 +73,51 @@ module OntimeDB
     results
   end
 
+  def self.city_arrv_delays
+    results = []
+
+    sql_most = %Q[
+      select origin_city_name, count(dep_delay_new) as delays
+      from on_time_perf
+      where arr_delay_new > 0
+      group by origin_city_name
+      order by delays desc
+      limit 1
+    ]
+
+    sql_least = %Q[
+      select origin_city_name, count(dep_delay_new) as delays
+      from on_time_perf
+      where arr_delay_new > 0
+      group by origin_city_name
+      order by delays asc
+      limit 1
+    ]
+
+    data = @db.exec sql_most
+    results << data.first
+    data = @db.exec sql_least
+    results << data.first
+    results
+  end
+
+  def self.avg_delay_all
+    results = []
+
+    sql = %Q[
+      select avg(dep_delay_new + arr_delay_new) as delays
+      from on_time_perf
+      where arr_delay_new > 0
+      or dep_delay_new > 0
+      order by delays desc
+    ]
+
+    @db.exec sql
+  end
+
 end
 
-#result = OntimeDB.city_depart_delays
+# result = OntimeDB.avg_late_all
 
-# result.each {|airline| puts airline["carrier"]}
-#p result
+# result.each {|airline| puts airline}
+#p result[""]
