d2230613e2d360f4bfe8a2de875837d0f2c52889
charles milam <charles.milam@gmail.com>
2014-12-04 00:27:13 -0600

    Add /airlines endpoint.

--- ontime_db_sinatra/server.rb
+++ ontime_db_sinatra/server.rb
@@ -1,6 +1,14 @@
 require "sinatra"
 require "./lib/ontime_db"
 
+
 get "/" do 
   erb :index
+end
+
+get "/airlines" do
+  #puts OntimeDB.methods
+  @content = OntimeDB.airlines
+
+  erb :airlines
 end
