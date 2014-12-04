02c62c1e588a4c708d20f8541bc6d724c450073f
charles milam <charles.milam@gmail.com>
2014-12-04 00:48:44 -0600

    Add database connection info. Modify method airlines to query and execute db.

--- ontime_db_sinatra/lib/ontime_db.rb
+++ ontime_db_sinatra/lib/ontime_db.rb
@@ -1,9 +1,25 @@
 require "pg"
 
 module OntimeDB
+  # connection to database
+  @db = PG::Connection.open(dbname: 'mks')
+  # database tables
+  @movies_table = "on_time_perf"
+
   def self.airlines
-    "airlines query"
+    sql = %[
+      select carrier, count(dep_delay_new) + count(arr_delay_new) as delays
+      from on_time_perf
+      where arr_delay_new > 0
+      and dep_delay_new > 0
+      group by carrier
+      order by delays desc
+    ]
+
+    @db.exec sql
   end
 end
 
-puts OntimeDB.methods
+# result = OntimeDB.airlines
+
+# result.each {|airline| puts airline["carrier"]}
