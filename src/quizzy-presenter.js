f0f736a7ac14dade2c25a991dbd494b7975314ef
charles milam <charles.milam@gmail.com>
2015-01-13 19:52:56 -0600

    Modify to add submit button to each question form.

--- src/quizzy-presenter.js
+++ src/quizzy-presenter.js
@@ -6,7 +6,14 @@
     question: "How much wood would a woodchuck chuck?",
     responses: ["3", "42","an inordinate amount","You woodchucks get away from my wood pile!"],
     answer: 1
-  }]
+    },
+    {
+      id: 2,
+      question: "What type of boat do you own?",
+      responses: ["Class C", "Commercial Fishing", "42", "a towel"],
+      answer: 2
+    }
+  ]
 
   console.log (quizzes)
   
@@ -23,10 +30,11 @@
       _.each(quiz.responses, function(response) {
         var radio = $("<input type='radio'>")
         var span = $("<span>").text(response)
-        quizResponses.append(radio)
-        quizResponses.append(span)
-        quizResponses.append($("<br>"))
+        $(".quiz-question").append(radio)
+        $(".quiz-question").append(span)
+        $(".quiz-question").append($("<br>"))
       })  
+      $(".quiz-question").append($("<button type='submit'>Answer Me</button>"))
     })
   }
 
