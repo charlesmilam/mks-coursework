f68ff7eb16b76f487e2fbdef899791888655dc1d
charles milam <charles.milam@gmail.com>
2015-01-13 20:46:30 -0600

    Modify to make use of model forEach method to retrieve data.

--- src/quizzy-presenter.js
+++ src/quizzy-presenter.js
@@ -1,26 +1,9 @@
 // Presenter
 (function () {
-  // test data
-  quizzes = [{
-    id: 1,
-    question: "How much wood would a woodchuck chuck?",
-    responses: ["3", "42","an inordinate amount","You woodchucks get away from my wood pile!"],
-    answer: 1
-    },
-    {
-      id: 2,
-      question: "What type of boat do you own?",
-      responses: ["Class C", "Commercial Fishing", "42", "a towel"],
-      answer: 2
-    }
-  ]
-
-  console.log (quizzes)
-  
   renderQuizzes()
   
   function renderQuizzes () {
-    _.each(quizzes, function(quiz) {
+    Quiz.forEach(function(quiz) {
       var question = $("<h3>").text(quiz.question)
       $(".quiz-question").append(question)
     
