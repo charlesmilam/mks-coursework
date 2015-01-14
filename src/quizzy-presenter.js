dff85a6026366541a5259c24c37731ab52f95fa0
charles milam <charles.milam@gmail.com>
2015-01-13 18:45:59 -0600

    Modify to not use ul for display of responses.

--- src/quizzy-presenter.js
+++ src/quizzy-presenter.js
@@ -1,37 +1,32 @@
 // Presenter
 (function () {
   // test data
-  quizzes = {
+  quizzes = [{
     id: 1,
     question: "How much wood would a woodchuck chuck?",
     responses: ["3", "42","an inordinate amount","You woodchucks get away from my wood pile!"],
     answer: 1
-  }
+  }]
 
   console.log (quizzes)
   
-  renderQuizs()
+  renderQuizzes()
   
-  function renderQuizs () {
+  function renderQuizzes () {
     var question = $("<h3>").text(quizzes.question)
     $(".quiz-question").append(question)
     
     
     //$(".quiz-responses").empty()
-    var responsesList = $(".responses-list")
+    var quizResponses = $(".quiz-responses")
       _.each(quizzes.responses, function(response) {
-        var radio = $("<input type='radio'>").text(response)
-        var li = $("<li>").append(radio)
+        var radio = $("<input type='radio'>")
+        var span = $("<span>").text(response)
+        quizResponses.append(radio)
+        quizResponses.append(span)
+        quizResponses.append($("<br>"))
         console.log(radio)
-        //console.log(response)
-        $(".responses-list").append(li)
-        responsesList.append(li)
       })
-        
-    
-    
-    //console.log(responsesList)
-    $(".quiz-responses").append(responsesList)
   }
 
 
