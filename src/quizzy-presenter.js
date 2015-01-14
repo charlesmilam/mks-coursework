8121c714598335144ac189c9420e5df0ff315e15
charles milam <charles.milam@gmail.com>
2015-01-13 18:53:21 -0600

    Modify data structure so more than one question may be asked.

--- src/quizzy-presenter.js
+++ src/quizzy-presenter.js
@@ -13,20 +13,21 @@
   renderQuizzes()
   
   function renderQuizzes () {
-    var question = $("<h3>").text(quizzes.question)
-    $(".quiz-question").append(question)
+    _.each(quizzes, function(quiz) {
+      var question = $("<h3>").text(quiz.question)
+      $(".quiz-question").append(question)
     
     
-    //$(".quiz-responses").empty()
-    var quizResponses = $(".quiz-responses")
-      _.each(quizzes.responses, function(response) {
+      //$(".quiz-responses").empty()
+      var quizResponses = $(".quiz-responses")
+      _.each(quiz.responses, function(response) {
         var radio = $("<input type='radio'>")
         var span = $("<span>").text(response)
         quizResponses.append(radio)
         quizResponses.append(span)
         quizResponses.append($("<br>"))
-        console.log(radio)
-      })
+      })  
+    })
   }
 
 
